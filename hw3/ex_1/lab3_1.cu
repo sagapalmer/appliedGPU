7384c72c42ddecdbc27ffa2d26621470f0040a1a
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2023-12-08 11:47:39 +0100

    update with working version

--- hw3/ex_1/lab3_1.cu
+++ hw3/ex_1/lab3_1.cu
@@ -8,52 +8,41 @@
 __global__ void histogram_kernel(unsigned int *input, unsigned int *bins,
                                  unsigned int num_elements,
                                  unsigned int num_bins) {
+  extern __shared__ unsigned int shared_bins[];
 
-  //@@ Insert code below to compute histogram of input using shared memory and atomics
-  extern __shared__ unsigned int s_bins[];
-  int s_idx = threadIdx.x;
+  unsigned int tid = threadIdx.x;
+  unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-
-  //initialize shared memory
-  if (s_idx < num_bins) { 
-    s_bins[s_idx] = 0; 
+  for (unsigned int j = tid; j < num_bins; j += blockDim.x) {
+      shared_bins[j] = 0;
   }
-
-  //wait for threads to zero out shared memory
   __syncthreads();
 
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-  if (idx < num_elements) {
-    int bin = input[idx];
-    atomicAdd(&s_bins[bin], 1U);
+  if (i < num_elements) {
+      atomicAdd(&shared_bins[input[i]], 1);
   }
-
-  //wait for threads to finish
   __syncthreads();
 
-  //write to global memory
-  if(s_idx < num_bins) { 
-    atomicAdd(&bins[s_idx], s_bins[s_idx]); 
+  // global memory
+  for (unsigned int j = tid; j < num_bins; j += blockDim.x) {
+      atomicAdd(&bins[j], shared_bins[j]);
   }
-
 }
 
 
 __global__ void convert_kernel(unsigned int *bins, unsigned int num_bins) {
-
   //@@ Insert code below to clean up bins that saturate at 127
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx < num_bins) {
-    if (bins[idx] > 127U) {
-      bins[idx] = 127U;
+  unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < num_bins) {
+    if (bins[i] > 127) {
+      bins[i] = 127;
     }
   }
 }
 
 
 int main(int argc, char **argv) {
-  
+
   int inputLength;
   unsigned int *hostInput;
   unsigned int *hostBins;
@@ -62,87 +51,111 @@ int main(int argc, char **argv) {
   unsigned int *deviceBins;
 
   //@@ Insert code below to read in inputLength from args
+  if (argc != 2) {
+    fprintf(stderr, "Usage: [input length]\n");
+    return 1;
+  }
+
   inputLength = atoi(argv[1]);
 
   printf("The input length is %d\n", inputLength);
-  
+
   //@@ Insert code below to allocate Host memory for input and output
-  hostInput = (unsigned int *)malloc(inputLength * sizeof(unsigned int));
-  hostBins = (unsigned int *)malloc(NUM_BINS * sizeof(unsigned int));
-  resultRef = (unsigned int *)malloc(NUM_BINS * sizeof(unsigned int));
+  hostInput = (unsigned int *) malloc(inputLength * sizeof(unsigned int));
+  hostBins = (unsigned int *) malloc(NUM_BINS * sizeof(unsigned int));
 
-  //cudaMallocManaged(&input, inputLength * sizeof(unsigned int));
-  //cudaMallocManaged(&bins, NUM_BINS * sizeof(unsigned int));
 
-  
   //@@ Insert code below to initialize hostInput to random numbers whose values range from 0 to (NUM_BINS - 1)
-  std::random_device rd; 
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<> randomValue(0, NUM_BINS-1);
-  for (int i = 0; i < inputLength; i++) {
-    hostInput[i] = randomValue(gen);
+  srand(time(NULL));
+  for (int i = 0; i < inputLength; ++i) {
+    hostInput[i] = rand() % (NUM_BINS - 1);
   }
 
-  /*
-  for (int i; i < inputLength; i++) {
-    hostInput[i] = rand() % NUM_BINS;
-  }
-  */
-
- 
   //@@ Insert code below to create reference result in CPU
-  memset(resultRef, 0, NUM_BINS * sizeof(unsigned int));
-  //memset(hostBins, 0, NUM_BINS * sizeof(unsigned int));
-  for (int i = 0; i < inputLength; i++) {
-    int bin = hostInput[i];
-    if (resultRef[bin] < 127) {
-      resultRef[bin] += 1;
-    }
+  resultRef = (unsigned int *) malloc(NUM_BINS * sizeof(unsigned int));
+  for (unsigned int i = 0; i < NUM_BINS; ++i) {
+    resultRef[i] = 0;
+  }
+  for (unsigned int i = 0; i < inputLength; ++i) {
+      if (resultRef[hostInput[i]] < 127) {
+          resultRef[hostInput[i]]++;
+      }
   }
 
   //@@ Insert code below to allocate GPU memory here
-  cudaMalloc(&deviceInput, inputLength * sizeof(unsigned int));
-  cudaMalloc(&deviceBins, NUM_BINS * sizeof(unsigned int));
+  cudaMalloc((void **) &deviceInput, inputLength * sizeof(unsigned int));
+  cudaMalloc((void **) &deviceBins, NUM_BINS * sizeof(unsigned int));
+
 
   //@@ Insert code to Copy memory to the GPU here
   cudaMemcpy(deviceInput, hostInput, inputLength * sizeof(unsigned int), cudaMemcpyHostToDevice);
-  //cudaMemcpy(deviceBins, hostBins, NUM_BINS * sizeof(unsigned int), cudaMemcpyHostToDevice);
+  cudaMemset(deviceBins, 0, NUM_BINS * sizeof(unsigned int));
+
 
   //@@ Insert code to initialize GPU results
   cudaMemset(deviceBins, 0, NUM_BINS * sizeof(unsigned int));
 
-
   //@@ Initialize the grid and block dimensions here
-  int TPB = 256;
-  int BLOCKS = (inputLength  + TPB - 1) / TPB;
-
-  // set the size of shared memory
-  int smemSize = (TPB)*sizeof(unsigned int);
+  dim3 blockDim(256);
+  dim3 gridDim((inputLength + blockDim.x - 1) / blockDim.x);
 
   //@@ Launch the GPU Kernel here
-  histogram_kernel<<<BLOCKS, TPB, smemSize>>>(deviceInput, deviceBins, inputLength, NUM_BINS);
-  cudaDeviceSynchronize();
+  histogram_kernel<<<gridDim, blockDim, NUM_BINS * sizeof(unsigned int)>>>(deviceInput, deviceBins, inputLength, NUM_BINS);
+  cudaError_t cudaStatus = cudaGetLastError();
+  if (cudaStatus != cudaSuccess) {
+    fprintf(stderr, "histogram kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+    return 1;
+  }
 
+  cudaStatus = cudaDeviceSynchronize();
+  if (cudaStatus != cudaSuccess) {
+    fprintf(stderr, "cuda sync for histogram kernel failed. error code %d\n", cudaStatus);
+    return 1;
+  }
 
   //@@ Initialize the second grid and block dimensions here
-  int TPB_2 = 256;
-  int BLOCKS_2 = (inputLength  + TPB_2 - 1) / TPB_2;
-
+  dim3 blockDim2(256);
+  dim3 gridDim2((NUM_BINS + blockDim2.x - 1) / blockDim2.x);
 
   //@@ Launch the second GPU Kernel here
-  convert_kernel<<<BLOCKS_2, TPB_2>>>(deviceBins, NUM_BINS);
-  cudaDeviceSynchronize();
+  convert_kernel<<<gridDim2, blockDim2>>>(deviceBins, NUM_BINS);
 
+  cudaError_t cudaStatus2 = cudaGetLastError();
+  if (cudaStatus2 != cudaSuccess) {
+    fprintf(stderr, "convert kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+    return 1;
+  }
+
+  cudaStatus2 = cudaDeviceSynchronize();
+  if (cudaStatus2 != cudaSuccess) {
+    fprintf(stderr, "cuda sync for convert kernel failed. error code %d\n", cudaStatus);
+    return 1;
+  }
 
   //@@ Copy the GPU memory back to the CPU here
   cudaMemcpy(hostBins, deviceBins, NUM_BINS * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
-
   //@@ Insert code below to compare the output with the reference
-  for (int i = 0; i < NUM_BINS; ++i) {
-    if (fabs(hostBins[i] - resultRef[i]) > 1e-5) {
-        fprintf(stderr, "hostBins and resultRef does not match on index %d: %u (GPU) != %u (CPU)\n", i, hostBins[i], resultRef[i]);
+  bool correct = true;
+  for (unsigned int i = 0; i < NUM_BINS; i++) {
+    if (hostBins[i] != resultRef[i]) {
+      fprintf(stderr, "BIN %u: GPU %u != CPU %u\n", i, hostBins[i], resultRef[i]);
+      correct = false;
+      break;
     }
+  }
+
+  if (correct) {
+    printf("GPU results match the reference results!\n");
+  } else {
+    printf("GPU results do not match reference!\n");
+  }
+
+  printf("Reference Histogram Results:\n");
+
+  printf("Bin,Count\n");
+  for (unsigned int i = 0; i < NUM_BINS; i++) {
+      printf("%u,%u\n", i, resultRef[i]);
   }
 
   //@@ Free the GPU memory here
@@ -156,4 +169,3 @@ int main(int argc, char **argv) {
 
   return 0;
 }
-
