96bc45a9f7eb525f7ca71eb04f074638338eaa6b
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-01-04 20:15:53 +0100

    add support for specifying S_seg

--- hw4/ex2_1.cu
+++ hw4/ex2_1.cu
@@ -27,15 +27,16 @@ double getElapsedTime(double startTime) {
 
 
 int main(int argc, char **argv) {
-  
+
   int inputLength;
+  int S_seg;
 
   //CPU
   DataType *hostInput1;
   DataType *hostInput2;
   DataType *hostOutput;
   DataType *resultRef;
-  
+
   //GPU
   DataType *deviceInput1;
   DataType *deviceInput2;
@@ -43,16 +44,17 @@ int main(int argc, char **argv) {
 
   //@@ Insert code below to read in inputLength from args
   inputLength = atoi(argv[1]); //convert char to int
+  S_seg = atoi(argv[2]);
 
   printf("The input length is %d\n", inputLength);
-  
+
   //@@ Insert code below to allocate Host memory for input and output
   hostInput1 = (DataType *)malloc(inputLength * sizeof(DataType));
   hostInput2 = (DataType *)malloc(inputLength * sizeof(DataType));
   hostOutput = (DataType *)malloc(inputLength * sizeof(DataType));
   resultRef = (DataType *)malloc(inputLength * sizeof(DataType));
 
-  
+
   //@@ Insert code below to initialize hostInput1 and hostInput2 to random numbers, and create reference result in CPU
   double startTimeCPU = getTime();
   for (int i = 0; i < inputLength; i++) {
@@ -61,34 +63,43 @@ int main(int argc, char **argv) {
         resultRef[i] = hostInput1[i] + hostInput2[i];
   }
   double elapsedTimeCPU = getElapsedTime(startTimeCPU);
-  
 
-  
-  
-  cudaStream_t streams[4];
-  for (int i = 0; i < 4; i++) {
-    cudaStreamCreate(&streams[i]);
-  }
 
-  
-  //@@ Insert code to below to Copy memory to the GPU here
-  
-  //@@ Initialize the 1D grid and block dimensions here
-  int S_seg = 10;
-  int streamSize = inputLength / S_seg;
-  int streamBytes = streamSize * sizeof(DataType);
-  
-  int TPB = 128;
-  int gridSize = (streamSize + TPB - 1) / TPB;
-  
   //@@ Insert code below to allocate GPU memory here
-  double startTimeGPU = getTime();
   cudaMalloc((void **)&deviceInput1, inputLength * sizeof(DataType));
   cudaMalloc((void **)&deviceInput2, inputLength * sizeof(DataType));
   cudaMalloc((void **)&deviceOutput, inputLength * sizeof(DataType));
-  
-  
+
+  cudaStream_t streams[S_seg];
+  for (int i = 0; i < S_seg; i++) {
+    cudaStreamCreate(&streams[i]);
+  }
+
+
+
+
+  //@@ Insert code to below to Copy memory to the GPU here
+  // double startDataCopyFromHostToDevice = getTime();
+  // //cudaMemcpy(deviceInput1, hostInput1, inputLength * sizeof(DataType), cudaMemcpyHostToDevice);
+  // //cudaMemcpy(deviceInput2, hostInput2, inputLength * sizeof(DataType), cudaMemcpyHostToDevice);
+
+  // cudaMemcpyAsync(deviceInput1, hostInput1, inputLength * sizeof(DataType), cudaMemcpyHostToDevice);
+  // cudaMemcpyAsync(deviceInput2, hostInput2, inputLength * sizeof(DataType), cudaMemcpyHostToDevice);
+  // double endDataCopyFromHostToDevice = getElapsedTime(startDataCopyFromHostToDevice);
+
+  // printf("Data copy from host to device: %.6f seconds\n", endDataCopyFromHostToDevice);
+
+  //@@ Initialize the 1D grid and block dimensions here
+  int streamSize = inputLength / S_seg;
+  int streamBytes = streamSize * sizeof(DataType);
+
+  int TPB = 128;
+  int gridSize = (streamSize + TPB - 1) / TPB;
+
   //@@ Launch the GPU Kernel here
+
+
+  double startTimeGPU = getTime();
   for (int i = 0; i < S_seg; i++) {
     int offset = i * streamSize;
 
@@ -113,7 +124,7 @@ int main(int argc, char **argv) {
   printf("Total time: %.6f seconds\n", elapsedTimeGPU);
 
   //@@ Insert code below to compare the output with the reference
-  printf("Elapsed Time for CPU: %.6f seconds\n", elapsedTimeCPU);
+  //printf("Elapsed Time for CPU: %.6f seconds\n", elapsedTimeCPU);
   for (int i = 0; i < inputLength; ++i) {
     if (std::abs(hostOutput[i] - resultRef[i]) > 1e-5) {
         fprintf(stderr, "hostOutput and resultRef does not match on index %d: %f (GPU) != %f (CPU)\n", i, hostOutput[i], resultRef[i]);
